77070df567706dabfb2cbd19f51fc2938d503777
igord <igordaniel11.id@gmail.com>
2025-11-19 17:23:09 -0300

    chore: configuração de pom.xml com JaCoCo e application.yml com profiles

---
+++ src/main/java/com/example/acess_request_manager/AcessRequestManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.acess_request_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessRequestManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessRequestManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/acess_request_manager/AcessRequestManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acess_request_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcessRequestManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
